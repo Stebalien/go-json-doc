cb7da25d75601985eb9cbe66aff5eb28251b8554
Steven Allen <steven@stebalien.com>
2019-04-29 17:31:40 -0700

    add helper types

---
+++ types.go
@@ -0,0 +1,7 @@
+package jsondoc
+
+// Object is a helper type for constructing JSON objects.
+type Object map[string]interface{}
+
+// Array is a helper type for constructing JSON arrays.
+type Array []interface{}
